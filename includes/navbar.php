24cf93c471b2d453c8569c30dac8046cf7e58042
sandeep singh bhati <bhatig901238@gmail.com>
2026-06-28 17:24:14 +0530

    day 1 & day 2 - project setup and work start

---
+++ includes/navbar.php
@@ -0,0 +1,60 @@
+<nav class="navbar navbar-expand-lg bg-white shadow-sm sticky-top">
+
+  <div class="container">
+
+    <a class="navbar-brand fw-bold" href="index.php">
+
+      <img src="assets/images/logo/logo.png" width="55">
+
+      SHREE RAM INTER COLLEGE
+
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbar">
+
+      <ul class="navbar-nav ms-auto align-items-lg-center">
+
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Academics</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Faculty</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Gallery</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Admission</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item ms-lg-3">
+          <a href="#" class="btn btn-primary rounded-pill px-4">
+            Admission Open
+          </a>
+        </li>
+
+      </ul>
+
+    </div>
+
+  </div>
+
+</nav>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once("includes/header.php");
+?>
+
+<?php
+require_once("includes/navbar.php");
+?>
+
+<div class="container py-5">
+
+<h1>Welcome to ABC Public School</h1>
+
+<p>Premium School Website</p>
+
+</div>
+
+<?php
+require_once("includes/footer.php");
+?>
